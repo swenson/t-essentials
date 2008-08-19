42d9f8841c8e9b531c3d22038883cc0d676234b4
root <root@chrisken.(none)>
2008-08-19 02:11:22 +0000

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :contracts
 
-  # map.resources :listings
+  map.resources :listings
 
   map.resources :users
 
@@ -47,9 +47,7 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
   
-  # map.root :controller => "contracts"
-
-  map.root :controller => "main"
+  map.root :controller => "contracts"
   
   # See how all your routes lay out with "rake routes"
 
