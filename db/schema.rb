8c849dba3d183d7dc110946501b14aca108b4f5d
Christopher Swenson <chris@caswenson.com>
2008-08-22 08:48:25 -0400

    Added main/show_page

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 26) do
+ActiveRecord::Schema.define(:version => 20080822122643) do
 
   create_table "ads", :force => true do |t|
     t.integer  "contract_id"
@@ -106,6 +106,13 @@ ActiveRecord::Schema.define(:version => 26) do
   create_table "modalities", :force => true do |t|
     t.string   "name"
     t.integer  "parent_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pages", :force => true do |t|
+    t.string   "name"
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
