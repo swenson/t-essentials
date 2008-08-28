79f89f8aba2c9910eac0cdbed2e86e5bb5e74f0c
Christopher Swenson <chris@caswenson.com>
2008-08-27 21:30:39 -0400

    Updated buttons!  Yay!

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function addButtonClasses()
+{
+  var elements = document.getElementsByTagName('input');
+
+  for (var i=0; i < elements.length; i++ )
+  {
+    if (elements[i].getAttribute('type'))
+    {
+      if (elements[i].getAttribute('type') == "submit")
+        elements[i].className = "submit";
+      else if (elements[i].getAttribute('type') == "button")
+        elements[i].className = "button";
+    }
+  }
+}
+
+window.onload = function() { addButtonClasses(); }
