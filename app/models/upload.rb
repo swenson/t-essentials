bb1f95f6941b631729593866154f5be901b0f238
Christopher Swenson <swenson@radiobook.local>
2008-10-02 19:18:09 -0600

    Amazon S3 FTW!

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,8 +1,7 @@
 class Upload < ActiveRecord::Base
   belongs_to :contract
   
-  has_attachment :storage => :file_system, 
-                 :path_prefix => 'public/files',
+  has_attachment :storage => :s3,
                  :size => 1..(10.megabytes)
 
   validates_as_attachment
