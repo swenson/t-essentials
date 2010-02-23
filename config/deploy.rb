c4249dfab9cc162a2da325864614034873496dce
Christopher Swenson <chris@caswenson.com>
2010-02-23 18:51:27 -0500

    Fixed deployment script a bit

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ set :application, "directory"
 set :repository,  "root@www.t-essentials.com:/home/git/directory"
 set :scm, :git
 set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
+set :use_sudo, false
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -16,4 +17,5 @@ set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 
 #role :app, "www.t-essentials.com"
 #role :web, "www.t-essentials.com"
-role :root,  "www.t-essentials.com", :primary => true
+#role :root,  "www.t-essentials.com", :primary => true
+server "www.t-essentials.com", :app, :web, :db, :primary => true
